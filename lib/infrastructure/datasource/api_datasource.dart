fe55f0ee72dff777701f6ca8c8411629d06e8c93
Anders@arbeigo.dk <Freesoft114>
2025-02-28 16:35:56 +0100

    update

--- lib/infrastructure/datasource/api_datasource.dart
+++ lib/infrastructure/datasource/api_datasource.dart
@@ -4,11 +4,12 @@ class ApiDataSource {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   CollectionReference get groupCollection => _firestore.collection('Groups');
+  CollectionReference get membershipCollection => _firestore.collection('Memberships');
+  CollectionReference get postCollection => _firestore.collection('Posts');
   CollectionReference get ticketTypeCollection => _firestore.collection('TicketTypes');
   CollectionReference get userCollection => _firestore.collection('Users');
   CollectionReference get messageCollection => _firestore.collection('Messages');
   Stream<QuerySnapshot> get messageStream => _firestore.collection('Messages').snapshots();
-  CollectionReference get membershipCollection => _firestore.collection('Memberships');
-  CollectionReference get postCollection => _firestore.collection('Posts');
+
 
 }
